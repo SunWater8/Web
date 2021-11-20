269bde0e1e6f5e7c25f1e6bb29eb17396e10ff69
silversoren <ehsoren9@gmail.com>
2021-11-21 02:40:37 +0900

    solution 적용해보기

--- nomad_chanllenge/assignments/submit/assignment02_event_classList/src/index.js
+++ nomad_chanllenge/assignments/submit/assignment02_event_classList/src/index.js
@@ -1,3 +1,4 @@
+/* 내가 직접해서 제출 
 function handleWindowResize() {
     const yellow = 'backgroundYellow';
     const blue = 'backgroundBlue';
@@ -22,10 +23,10 @@ function handleWindowResize() {
 }
 window.addEventListener('resize', handleWindowResize);
 
+ */
 
 
-
-/*
+/* 다른사람 예시
 function handleWindowResize2(){
     if(window.innerWidth < 400 ){
         document.body.style.backgroundColor = 'goldenrod';
@@ -39,3 +40,28 @@ function handleWindowResize2(){
 
 window.addEventListener('resize', handleWindowResize2);
  */
+
+//solution 본 후 다듬기
+const smallSize = 'backgroundYellow';
+const midSize = 'backgroundBlue';
+const largeSize = 'backgroundPurple';
+
+
+
+function handleWindowResize(){
+    const width = window.innerWidth;
+    const body = document.body;
+    if(width < 400){
+        body.classList.add(smallSize);
+        body.classList.remove(midSize);
+    }else if(width >=400 && window.innerWidth < 900){
+        body.classList.add(midSize);
+        body.classList.remove(smallSize, largeSize);
+    }else{
+        body.classList.add(largeSize);
+        body.classList.remove(midSize);
+    }
+    console.log(body.className);
+
+}
+window.addEventListener('resize', handleWindowResize);
